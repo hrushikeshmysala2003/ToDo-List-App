465c60a0384b026fd8be7dab3836dc326fd7fbfd
hrushikesh2003 <125362034+hrushikeshmysala2003@users.noreply.github.com>
2023-07-06 23:03:55 +0530

    Add files via upload
    Added Index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+let listContainer = document.querySelector(".list-container");
+
+const addTask =  () => {
+    if(inputBox.value === ""){
+        alert("You must write something!")
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+};
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+// window.onresize = function() {
+//      document.querySelector("html").fon
+// };
